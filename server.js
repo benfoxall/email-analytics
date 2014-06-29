e13c548c8434795027ba88276fd38f04a0d7a9be
Ben Foxall <benfoxall@gmail.com>
2014-06-29 13:37:25 +0100

    heroku deploying

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var port = process.env.PORT || 3000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World (through travis)\n');
 }).listen(port);
