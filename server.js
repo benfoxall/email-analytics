2a7ed89536ea2be93d32724a1a40de6b01e61920
Ben Foxall <benfoxall@gmail.com>
2014-06-29 16:57:24 +0100

    persistence

--- server.js
+++ server.js
@@ -1,11 +1,61 @@
 var express = require('express'),
+	bodyParser = require('body-parser'),
+	redis = require("redis");
+
+require("redis-scanstreams")(redis);
+
+var
+	client = redis.createClient(process.env.REDISTOGO_URL),
 	app = express();
 
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.send('hello world (express)');
-});
+app.use(bodyParser.json())
+
+app.use(express.static(__dirname + '/public'));
+
+
+/* 
+	Set a value for a given key
+	POST /set/keyname {value:20 }
+*/
+app.post('/set/:key', function(req,res){
+
+	var key = req.param('key'),
+		value = req.param('value'),
+		now = +new Date;
+
+	if(value)client.zadd(key, -now, [now,value].join(' '))
+
+	res.send(
+		value ? 200 : 403,
+		{status: value ? 'ok' : 'missing key'}
+	);
+
+})
+
+app.post('/clear/:key', function(req,res){
+	client.del(req.param('key'))
+	res.send({status:'cleared'})
+})
+
+app.get('/data/:key', function(req, res){
+
+	var keys = [];
+
+	client.zscan(req.param('key'), {count: 3})
+	.on('data', function(s){
+		keys.push(
+			s.key.split(' ').map(function(n){
+				return parseInt(n,10)
+			}));
+	})
+	.on('end', function(){
+		res.send(keys)	
+	})
+	
+
+})
 
 app.listen(port);
 
--- test/app_test.js
+++ test/app_test.js
@@ -1,5 +1,7 @@
-var request = require('supertest')
-  , app = require('../server.js');
+var request = require('supertest'),
+	redis = require("redis"),
+	client = redis.createClient(),
+  	app = require('../server.js');
 
 
 describe('app', function(){
@@ -7,7 +9,56 @@ describe('app', function(){
 		request(app)
 			.get('/')
 			.expect('Content-Type', /text/)
-			.expect(200, /hello world/)
+			.expect(200, /hello/)
 			.end(done);
 	})
+
+
+	describe('pushing values', function(){
+	
+		it('can set foo', function(done){
+			request(app)
+				.post('/set/foo', {value:10})
+				.expect('Content-Type', /application\/json/)
+				.end(done)
+		})
+		
+		it('can errors without value', function(done){
+			request(app)
+				.post('/set/bar')
+				.expect('Content-Type', /application\/json/)
+				.expect(403)
+				.end(done)
+		})
+		
+	})
+
+
+	describe('clearing values', function(){
+	
+		it('can clear foo', function(done){
+			request(app)
+				.post('/clear/foo')
+				.expect('Content-Type', /application\/json/)
+				.end(done)
+		})
+
+	});
+
+	describe('persistence', function(){
+		before(function(done){
+			request(app)
+				.post('/set/persist', {value:10})
+				.end(done)
+		})
+
+		before(function(done){
+			request(app)
+				.post('/set/persist', {value:20})
+				.end(done)
+		})
+
+		// xit('persisted in redis', function(done){})
+	})
+
 })
