4154bfa97d1b4469d9451e6d781337c97fee4c92
Ben Foxall <benfoxall@gmail.com>
2014-06-29 13:22:05 +0100

    herokufy

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+var port = process.env.PORT || 3000;
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port);
