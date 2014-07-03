7150b52d5a47c9a183afdbc7632fb60aa1cb4662
Ben Foxall <benfoxall@gmail.com>
2014-07-03 07:33:31 +0100

    added cache headers

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use(bodyParser.urlencoded())
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Cache-Control", "public, max-age=600"); //ten minutes
   next();
  });
 
